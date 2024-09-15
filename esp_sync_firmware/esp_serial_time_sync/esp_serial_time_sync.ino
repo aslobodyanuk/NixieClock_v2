eb57877f202327ce8a9bf51ac4db282d7d9a7932
Andrii Slobodianiuk <andrii.slobodianiuk@angloamerican.com>
2024-09-15 18:18:54 +0200

    Implemented configuration portal to allow changing wifi ap without programming

--- esp_sync_firmware/esp_serial_time_sync/esp_serial_time_sync.ino
+++ esp_sync_firmware/esp_serial_time_sync/esp_serial_time_sync.ino
@@ -1,54 +1,67 @@
 #include <NTPClient.h>
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
-
-const char *ssid     = "Internet Puhalo";
-const char *password = "V8UpvvZoic";
+#include <SimplePortal.h>
+#include <FileData.h>
+#include <LittleFS.h>
 
 // Get timezone from https://github.com/nayarsystems/posix_tz_db/blob/master/zones.csv
 #define NTP_SERVER "pool.ntp.org"
 #define TIMEZONE "CET-1CEST,M3.5.0,M10.5.0/3"
 
-// #define UTC_OFFSET_IN_SECONDS 7200
-// #define UPDATE_TIME_EVERY_MILLIS 1 * 60 * 1000
-
 #define LED_PIN 2
 
-unsigned long _lastUpdatedTime = 0;
-unsigned long _wifiConnectionTimer = 0; 
+#define CONFIG_PORTAL_TIMEOUT_MS 60000
 
-// WiFiUDP ntpUDP;
-// NTPClient timeClient(ntpUDP, "pool.ntp.org", UTC_OFFSET_IN_SECONDS);
+#define STATUS_CONFIG_PORTAL 0
+#define STATUS_WIFI_CONNECTING 1
+#define STATUS_WIFI_CONNECT 2
+
+unsigned long _lastUpdatedTime = 0;
+unsigned long _wifiConnectionTimer = 0;
+unsigned long _configPortalTimer = 0;
+unsigned long _configPortalTimoutTimer = 0;
+byte _status = STATUS_CONFIG_PORTAL;
+
+struct ConnectionData {
+  char ssid[32];
+  char pswd[32];
+};
+ConnectionData _connectionData;
+
+FileData _connectionFileData(&LittleFS, "/conn_data.dat", 'B', &_connectionData, sizeof(_connectionData));
 
 void setup(){
   Serial.begin(115200);
-  delay(2000);
-
-  Serial.println("<dbg>Connecting to WiFi");
+  delay(3000);
+  Serial.println("Initializing ESP.");
 
   pinMode(LED_PIN, OUTPUT);
-
+  initializeConnectionData();
   configTime(TIMEZONE, NTP_SERVER);
-
-  connectToWifi();
-
-  Serial.println("</dbg>}");
-
-  // timeClient.begin();
+  portalStart();
 }
 
 void loop() {
+  _connectionFileData.tick();
 
-  // if (timePeriodIsOver(_lastUpdatedTime, UPDATE_TIME_EVERY_MILLIS)) {
-  //   timeClient.update();    
-  // }
+  if (_status == STATUS_CONFIG_PORTAL && timePeriodIsOver(_configPortalTimer, 500)) {
+    indicatePortal();
+  }
 
-  // String formattedTime = timeClient.getFormattedTime();
-  // Serial.print("Formatted Time: ");
-  // Serial.println(formattedTime);  
+  if (_status == STATUS_CONFIG_PORTAL && timePeriodIsOver(_configPortalTimoutTimer, CONFIG_PORTAL_TIMEOUT_MS)) {
+    Serial.println("Configuration portal timeout.");
+    connectToWifi();
+  }
 
-  printTime();
-  delay(1000);
+  if (_status == STATUS_CONFIG_PORTAL && portalTick()) {
+    connectToWifi();
+  }
+
+  if (_status == STATUS_WIFI_CONNECT) {
+    printTime();
+    delay(1000);
+  }
 }
 
 void printTime() {
@@ -85,11 +98,48 @@ void indicateConnectingToWiFi() {
   blinkLed(100);
 }
 
-void connectToWifi() {
-  WiFi.persistent(false);
-  WiFi.mode(WIFI_STA);
+void indicatePortal() {
+  blinkLed(500);
+}
 
-  WiFi.begin(ssid, password);
+void initializeConnectionData() {
+  LittleFS.begin();
+  _connectionFileData.read();
+  printConnectionData();
+}
+
+void connectToWifi() {
+  _status = STATUS_WIFI_CONNECTING;
+
+  Serial.print("Portal status: ");
+  Serial.println(portalStatus());
+  // status: 0 error, 1 connect, 2 ap, 3 local, 4 exit, 5 timeout
+  portalStop();
+  
+  if (portalStatus() == SP_SUBMIT) {
+    Serial.println("Got new WiFi config from portal.");
+    Serial.println(portalCfg.SSID);
+    Serial.println(portalCfg.pass);
+
+    strcpy(_connectionData.ssid, portalCfg.SSID);
+    strcpy(_connectionData.pswd, portalCfg.pass);
+    _connectionFileData.updateNow();
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(portalCfg.SSID, portalCfg.pass);
+    WiFi.setAutoReconnect(true);
+    WiFi.persistent(true);
+  }
+
+  if (portalStatus() != SP_SUBMIT) {
+    Serial.println("Connecting to previous WiFi.");
+    printConnectionData();
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(_connectionData.ssid, _connectionData.pswd);
+    WiFi.setAutoReconnect(true);
+    WiFi.persistent(true);
+  }
 
   while (WiFi.status() != WL_CONNECTED) {
     yield();
@@ -106,6 +156,16 @@ void connectToWifi() {
     Serial.print("Connected to network: ");
     Serial.println(WiFi.localIP());
   }
+
+  _status = STATUS_WIFI_CONNECT;
+}
+
+void printConnectionData() {
+  Serial.print("Saved ssid: ");
+  Serial.println(_connectionData.ssid);
+
+  Serial.print("Saved pswd: ");
+  Serial.println(_connectionData.pswd);
 }
 
 boolean timePeriodIsOver(unsigned long & periodStartTime, unsigned long timePeriod) {
